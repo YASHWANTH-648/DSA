0a1124058095dd0094b61a5962937622bd03ffe4
yash-25 <2311cs010648@mallareddyuniversity.ac.in>
2025-01-24 22:40:58 +0530

    Update array2.cpp

--- array2.cpp
+++ array2.cpp
@@ -5,16 +5,14 @@ using namespace std;
 int main(){
 int arr[]={1,9,6,3,7,5,8,6};
     int Large=arr[0];
- for (int i = 1; i < 5; i++){
-if (arr[i]>Large){
-    Large=arr[i];
-}}
-int SecondLarge=-1;
-for (int  i = 0; i < 5; i++){
-    if (arr[i]>SecondLarge && arr[i]!=Large){
-        SecondLarge=arr[i];
+int SeondLarge=-1;
+    for(int i=1;i<8;i++){
+        if(arr[i]>Large){
+            SeondLarge=Large;
+            Large=arr[i];
+            
     }
-}
+
 cout<<"Second Largest elemnt "<<endl<<SecondLarge;
     return 0;
 }
